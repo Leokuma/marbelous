b5bb178747c2de1a97a6b728410514d5b7bc3233
Leonardo Okuma <leonardo.okuma@gmail.com>
2024-03-10 00:52:20 -0300

    First commit

---
+++ generate_marble.ts
@@ -0,0 +1,65 @@
+const RADIUS = 500;
+const DIAMETER = RADIUS * 2;
+const CANVAS_SIZE = 5000;
+const CENTER = CANVAS_SIZE / 2;
+
+interface MarbleOptions {
+	/** Positive number between 0 and 1 */
+	blur?: number
+	circles?: number
+	colors?: string[]
+	/** Positive number between 0 and 1 */
+	curveness?: number
+}
+
+interface Circle {
+	r: number
+	cx: number
+	cy: number
+}
+
+function generateMarble(options?: MarbleOptions): string {
+	const virtualCanvasSize = (options?.curveness ? Math.round(CANVAS_SIZE * options.curveness) : CANVAS_SIZE);
+
+	let nCircles = (options?.circles || 4);
+	const circles: Circle[] = [];
+	while (--nCircles)
+		circles.push(randCircle(virtualCanvasSize));
+
+	return `
+		<svg viewBox="${CENTER - RADIUS} ${CENTER - RADIUS} ${DIAMETER} ${DIAMETER}">
+			<defs>
+				<mask id="mask">
+					<circle cx="${CENTER}" cy="${CENTER}" r="${RADIUS}" fill="#fff"/>
+				</mask>
+			</defs>
+
+			<g mask="url(#mask)">
+				<circle r="${RADIUS}" cx="${CENTER}" cy="${CENTER}" fill="${randColor()}"/>
+				${circles.map(circle => `<circle r="${circle.r}" cx="${circle.cx}" cy="${circle.cy}" fill="${randColor()}"/>`).join('')}
+			</g>
+		</svg>
+	`.replaceAll(/[\n\t]/g, '');
+}
+
+function randCircle(virtualCanvasSize: number): Circle {
+	const cx = Math.round(Math.random() * virtualCanvasSize);
+	const cy = Math.round(Math.random() * virtualCanvasSize);
+
+	const x = Math.abs(CENTER - cx);
+	const y = Math.abs(CENTER - cy);
+	const r = Math.max(0, Math.round(Math.sqrt(x ** 2 + y ** 2)) - RADIUS) + Math.round(Math.random() * DIAMETER);
+
+	return {r, cx, cy};
+}
+
+function randColor() {
+	return '#' + randColorComponent() + randColorComponent() + randColorComponent() + randColorComponent();
+}
+
+function randColorComponent() {
+	return (Math.round(255 * Math.random())).toString(16).padStart(2, '0');
+}
+
+
+export default generateMarble;
